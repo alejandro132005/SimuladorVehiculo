378b8ff2cf564c55e0f15eb521b88200722bf49e
alejandro132005 <alejandrogp1395@gmail.com>
2025-04-06 19:42:58 -0500

    creacion de paquete y clases models

---
+++ src/autonoma/simuladorVehiculo/models/Motor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.simuladorVehiculo.models;
+
+/**
+ *
+ * @author aleja
+ */
+public class Motor {
+    
+}
